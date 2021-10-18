f4bb7fd09701a1f1b18fcf784c0763168780304d
Andrés Felipe Martínez Salazar <andresfelipe.2031@gmail.com>
2021-10-17 21:24:11 -0500

    debug changed for production

--- supermarket/settings.py
+++ supermarket/settings.py
@@ -25,7 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', True)
+if not isinstance(DEBUG, bool):
+    DEBUG = False
 
 ALLOWED_HOSTS = []
 
